4c3727304d77177180e407db83cd818af2e2832c
recurze <vrahul24999@gmail.com>
2023-02-20 01:00:19 +0100

    Set qf list with lsp diagnostics

--- nvim/lua/lsp.lua
+++ nvim/lua/lsp.lua
@@ -1,19 +1,18 @@
-
 local on_attach = function(client, bufnr)
     vim.api.nvim_buf_set_option(bufnr, 'tagfunc', 'v:lua.vim.lsp.tagfunc')
     vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
 
-    -- The audacity, when I didn't ask for it.
-    vim.diagnostic.disable()
-
     -- Mappings.
-    local bufopts = { noremap=true, silent=true, buffer=bufnr }
+    local bufopts = { noremap = true, silent = true, buffer = bufnr }
     vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
     vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
     vim.keymap.set('n', 'td', vim.lsp.buf.type_definition, bufopts)
 
     vim.api.nvim_create_user_command('Rename', function() vim.lsp.buf.rename() end, {})
-    vim.api.nvim_create_user_command('Diagno', function() vim.diagnostic.enable() end, {})
+
+    -- The audacity! When I didn't ask for it.
+    vim.diagnostic.disable()
+    vim.api.nvim_create_user_command('Diag', function() vim.diagnostic.setqflist() end, {})
 end
 
 require 'lspconfig'.clangd.setup{
